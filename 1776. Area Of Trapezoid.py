cf9ba032e770992b4bba726a28a84237ffa3f017
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-08-07 10:49:30 +0200

    Create 1776. Area Of Trapezoid.py

---
+++ 1776. Area Of Trapezoid.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    @param a: 
+    @param b: 
+    @param h: 
+    @return: Return the area of trapezoid
+    """
+    def AreaOfTrapezoid(self, a, b, h):
+        return (a+b)*h/2
