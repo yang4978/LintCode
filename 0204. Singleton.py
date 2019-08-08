1a83fdd32f0e0c3003bfa5daee4e4eb8f079aa53
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-08-08 10:21:43 +0200

    Create 0204. Singleton.py

---
+++ 0204. Singleton.py
@@ -0,0 +1,8 @@
+class Solution:
+    # @return: The same instance of this class every time
+    instance = None
+    @classmethod
+    def getInstance(cls):
+        if cls.instance is None:
+            cls.instance = Solution()
+        return cls.instance
